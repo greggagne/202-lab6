cc3c3bacda875a39d7bfe80dadf91f01fb85e790
greggagne <ggagne@westminsteru.edu>
2024-02-13 17:26:15 -0700

    Added ListQueue and ArrayQueue classes.

---
+++ src/ArrayQueue.java
@@ -0,0 +1,21 @@
+public class ArrayQueue<T> implements Queue<T> {
+    @Override
+    public void add(T item) {
+
+    }
+
+    @Override
+    public T remove() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
---
+++ src/ListQueue.java
@@ -0,0 +1,21 @@
+public class ListQueue<T> implements Queue<T> {
+    @Override
+    public void add(T item) {
+
+    }
+
+    @Override
+    public T remove() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
